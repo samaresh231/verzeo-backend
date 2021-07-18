b09b3187418f5818e74673bb2e1b16da8dd788f5
samaresh231 <47944092+samaresh231@users.noreply.github.com>
2021-07-18 12:17:58 +0530

    removed cors

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ const { v4: uuidv4 } = require('uuid')
 
 require('./authenticate');
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-});
-
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
